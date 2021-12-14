c0edd2a30c81a27cd694a76e59dd07a21974aa10
shadikul bari <shadikul.bari@bjitgroup.com>
2021-12-14 23:42:40 +0600

    Initial commit

---
+++ app/src/main/java/com/sadikul/currencyconverter/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.sadikul.currencyconverter.utils
+
+import com.sadikul.currencyconverter.BuildConfig
+
+object Constants {
+    const val DATABASE_NAME = "MLBDGallery.db"
+    val PREFERENCE= BuildConfig.APPLICATION_ID + ".PREFERENCE";
+}
